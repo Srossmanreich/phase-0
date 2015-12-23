a5dc8b2d93fd8c6e9a33813e338d9131aa8d171d
Srossmanreich <Srossmanreich@gmail.com>
2015-12-22 21:01:49 -0800

    Update my_solution.rb

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -3,24 +3,6 @@
 
 # Your Solution Below
 
-=begin
-def valid_triangle?(a, b, c)
-  if (a==0) || (b==0) || (c==0)
-  	false
-  elsif (a**2 + c**2) == b**2
-  	true
-  elsif (b**2 + a**2) == c**2
-  	true
-  elsif (b**2 + c**2) == a**2
-  	true
-  elsif (a==b) && (b==c) && (a==c)
-  	true
-  else
-  	false
-  end
-end
-=end
-
 def valid_triangle?(a, b, c)
   if (a==0) || (b==0) || (c==0)
   	false
