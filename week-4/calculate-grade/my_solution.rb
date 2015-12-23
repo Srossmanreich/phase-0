1e3bf2293991ab8708bbf40ddecf6e29c284a62d
Sydney Rossman-Reich <srossmanreich@gmail.com>
2015-12-22 21:00:15 -0800

    Add content to flow challenges

--- week-4/calculate-grade/my_solution.rb
+++ week-4/calculate-grade/my_solution.rb
@@ -1,6 +1,20 @@
 # Calculate a Grade
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Coline.
 
 
 # Your Solution Below
+
+def get_grade(x)
+	if x > 90
+		return "A"
+	elsif x < 90 && x >= 80
+		return "B"
+	elsif x < 80 && x >= 70
+		return "C"
+	elsif x < 70 && x >= 60
+		return "D"
+	else
+		return "F"
+	end
+end
