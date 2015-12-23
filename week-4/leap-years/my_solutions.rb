3ad905533ab0ff321e3456fae21fe80447abca03
Sydney Rossman-Reich <srossmanreich@gmail.com>
2015-12-22 20:53:48 -0800

    Add 4.5 folders

--- week-4/leap-years/my_solutions.rb
+++ week-4/leap-years/my_solutions.rb
@@ -1,6 +1,17 @@
 # Leap Years
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge Coline.
 
 
 # Your Solution Below
+def leap_year?(x)
+	if (x % 4 == 0) && (x % 100 == 0) && ((x % 400) != 0)
+		false
+	elsif ((x - 2016) % 4 == 0)
+		true
+	elsif ((x-2016) % 100 == 0)
+		true
+	else
+		false
+	end
+end
