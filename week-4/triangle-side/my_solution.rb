24c70f8e1af8d2a989f461653b09654c0c82210a
Sydney Rossman-Reich <srossmanreich@gmail.com>
2015-12-22 21:01:20 -0800

    add triangle code

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -3,6 +3,40 @@
 
 # Your Solution Below
 
+=begin
 def valid_triangle?(a, b, c)
-  # Your code goes here!
+  if (a==0) || (b==0) || (c==0)
+  	false
+  elsif (a**2 + c**2) == b**2
+  	true
+  elsif (b**2 + a**2) == c**2
+  	true
+  elsif (b**2 + c**2) == a**2
+  	true
+  elsif (a==b) && (b==c) && (a==c)
+  	true
+  else
+  	false
+  end
+end
+=end
+
+def valid_triangle?(a, b, c)
+  if (a==0) || (b==0) || (c==0)
+  	false
+  elsif a < 0 || b < 0 || c < 0
+  	false
+  elsif (a==b) && (b==c) && (a==c)
+  	true
+  elsif a+b > c && b+c > a && a+c > b
+  	true
+   elsif (a**2 + c**2) == b**2
+  	true
+  elsif (b**2 + a**2) == c**2
+  	true
+  elsif (b**2 + c**2) == a**2
+  	true
+  else
+  	false
+  end
 end
