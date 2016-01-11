b05eb4de716841ca94e71dc4840f41f08bc381e4
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-01-11 18:56:58 -0500

    Add gps 2.3 answer

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,33 +1,37 @@
 # Your Names
-# 1)
-# 2)
+# 1) Sydney
+# 2) Matt
 
 # We spent [#] hours on this challenge.
 
+=begin
+What is this code actually trying to do?
+First definition: serving size calculator
++++++ it takes an item and number of ingredients
++++++ makes sure it is in the library (raises error if not)
++++++ serving size = value at item in the hash
++++++ remaining ingredients = num ingred % serving size 
+=end
+
 # Bakery Serving Size portion calculator.
 
-def serving_size_calc(item_to_make, num_of_ingredients)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+def serving_size_calc(item_to_make, num_of_servings)
+  desserts = {"cookie" => 1, "cake" =>  5, "pie" => 7}
+  raise ArgumentError.new("#{item_to_make} is not a dessert we serve") unless desserts.has_key?(item_to_make) 
+  
+  #-------------------------------------------------
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      error_counter += -1
-    end
-  end
+  serving_size = desserts[item_to_make]
+  remaining_servings = num_of_servings % serving_size
+  
+  extra = "cookie" if remaining_servings < 5
+  else extra = "cake"
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
-
-  serving_size = library.values_at(item_to_make)[0]
-  remaining_ingredients = num_of_ingredients % serving_size
-
-  case remaining_ingredients
+  case remaining_servings
   when 0
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
+    return "Calculations complete: Make #{num_of_servings / serving_size} #{item_to_make}"
   else
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    return "Calculations complete: Make #{num_of_servings / serving_size} #{item_to_make}. You still have #{remaining_servings} servings remaining. Suggested baking items: #{remaining_servings/desserts[extra]} of #{extra}"
   end
 end
 
@@ -37,6 +41,30 @@ p serving_size_calc("cake", 5)
 p serving_size_calc("cake", 7)
 p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
+p serving_size_calc("pie",12)
+
 p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
+
+=begin
+What did you learn about making code readable by working on this 
+challenge?
+It is important for code to be easily understood by others reading it
+as what might be intuitive or obvious for one person might not be for
+the next (including that same person but sitting at the code cold!)
+
+Did you learn any new methods? What did you learn about them?
+I used has_key for the first time which seems like a pretty useful
+method!
+
+What did you learn about accessing data in hashes? 
+It is fairly similar to arrays. You can use 'each' and the key as
+the 'index' to call values.
+
+What concepts were solidified when working through this challenge?
+I need to be better at naming my variables and be careful that my
+code is easy to read!
+
+=end
+
