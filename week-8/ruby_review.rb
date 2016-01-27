24563c6be34ab537e6a4f190ad1f850237bfc1ab
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-01-26 20:06:30 -0800

    Add ruby challenge

--- week-8/ruby_review.rb
+++ week-8/ruby_review.rb
@@ -1,98 +1,71 @@
-#Introduction to Inheritance - RUBY Review 8.6
-
-# Introduction to Inheritance
+# U2.W6: Testing Assert Statements
 
 # I worked on this challenge [by myself, with: ].
 
 
-# Pseudocode
+# 2. Review the simple assert statement
 
+def assert
+  raise "Assertion failed!" unless yield
+end
 
+name = "bettysue"
+assert { name == "bettysue" }
+assert { name == "billybob" }
 
+# 2. Pseudocode what happens when the code above runs
 
-# Initial Solution
+=begin
+It is defining a function assert that raises a fail
+unless the function yields.
 
-class GlobalCohort
+In the code below, the assert matching bettysue's name
+returns nil because there was no error, but when bettysue
+is matched with billybob there is and an error is raised.
+=end
 
-	attr_accessor :name, :p0_start_date, :immersive_start_date, :graduation_date
+# 3. Copy your selected challenge here
+
+# Calculate a letter grade!
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# I worked on this challenge [by myself, with: ].
+
+# 0. Pseudocode
+
+# What is the input?
+# What is the output? (i.e. What should the code return?)
+# What are the steps needed to solve the problem?
+
+def get_grade(array)
   
-  def initialize(name,p0_start_date,immersive_start_date,graduation_date)
-  	@name = name
-  	@p0_start_date = p0_start_date
-  	@immersive_start_date = @immersive_start_date
-  	@graduation_date = graduation_date
-  	@student_list = {"San Francisco" => [], "Chicago" => [], "New York" => []}
-  end
+  sum = 0
+  array.each{|x|sum+=x}
+  grade = sum/array.size
 
-  def students
-  	@student_list.each{|key,value| 
-		p value[0][0].nil? ? "" : value[0][0]
-	}
-  end
-
-  def add_student(stu_name,email,location)
-  	@stu_name = stu_name
-  	@email = email
-  	@location = location
-  	@student_list.each{|key,value|
-  		if key == @location
-  			value.push([@stu_name,@email])
-  		end
-  	}
-  end
-
-  def email_list
-  	@student_list.each{|key,value| 
-		p value[0][1].nil? ? "" : value[0][1]
-	}
-  end
-
-  def num_of_students
-  	p sum = @student_list["San Francisco"].length + @student_list["Chicago"].length + @student_list["New York"].length
-  end
+  return "A" if grade > 89
+  return "B" if grade > 79
+  return "C" if grade > 69
+  return "D" if grade > 59
+  "F"
 
 end
 
-test = GlobalCohort.new("Fiddler Crabs", "12/01/2015", "2/9/2016", "4/14/2016")
-test.add_student("Sydney","srossmanreich@gmail.com","San Francisco")
-test.add_student("Chelsea","chelsea@gmail.com","Chicago")
-test.add_student("Newton","newton@gmail.com","New York")
-test.students
-test.email_list
-test.num_of_students
 
+# 4. Convert your driver test code from that challenge into Assert Statements
 
-class LocalCohort < GlobalCohort
-
-	attr_accessor :city, :name
-  
-  def initialize(name,p0_start_date,immersive_start_date,graduation_date,city)
-  	@city = city
-  	super(name)
-  	super(p0_start_date)
-  	super(immersive_start_date)
-  	super(graduation_date)
-  	@name = name
-  	@p0_start_date = p0_start_date
-  	@immersive_start_date = immersive_start_date
-  	@graduation_date = graduation_date
-  end
-
-  def students
-  	@student_list.each{|key,value| 
-		p value[@city][0].nil? ? "" : value[@city][0]
-	}
-  end
-
+def assert
+  raise "Assertion failed!" unless yield
 end
 
-test2 = LocalCohort.new("Fiddler Crabs", "12/01/2015", "2/9/2016", "4/14/2016", "San Francisco")
+answer = get_grade([100,90,44,84])
+assert { answer == "A" }
+assert { answer == "B" }
+assert { answer == "C" }
+assert { answer == "D" }
+assert { answer == "F" }
 
-
-# Refactored Solution
-
-
-
-
-
-# Reflection
+# 5. Reflection
